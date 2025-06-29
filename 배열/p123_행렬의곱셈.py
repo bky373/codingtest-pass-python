dedadc25d5b38bc55f1a46b31032f1a1bcd08ba8
bky373 <bo373@naver.com>
2025-06-29 13:06:44 +0900

    p123_행렬의곱셈 2차 풀이

--- 배열/p123_행렬의곱셈.py
+++ 배열/p123_행렬의곱셈.py
@@ -1,18 +1,22 @@
+# 1 4  3 3
+# 3 2  3 3
+# 4 1
+
 arr1 = [[1,4], [3,2], [4,1]]
 arr2 = [[3,3], [3,3]]
 # arr1 = [[1, 2], [3, 4]]
 # arr2 = [[1, 1], [1, 1]]
-arr3 = [[0 for _ in range(len(arr2))] for _ in range(len(arr1))]
-
 
 def solution(arr1, arr2):
-    for k in range(len(arr2[0])):
-        for i in range(len(arr1)):
-            temp = 0
-            for j in range(len(arr2)):
-                temp += arr1[i][j] * arr2[j][k]
-            arr3[i][k] = temp
-    return arr3
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
 
+    ret = [[0] * c2 for _ in range(r1)]
+
+    for i in range(r1):
+        for j in range(c2): # c1 돌고, c2 돈다
+            for k in range(c1):
+                ret[i][j] += arr1[i][k] * arr2[k][j]
+    return ret
 
 print(solution(arr1, arr2))
